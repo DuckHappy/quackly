8a48ecfc801427eba82c63524b0424425a2a469b
vjaviertaype <168123371+vjaviertaype@users.noreply.github.com.>
2025-09-28 15:17:48 -0300

    fix(imports): se arreglo las importaciones

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { CommunitiesModule } from './communities/communities.module';
-import { RoleModule } from './role/role.module';
 import { MemberModule } from './member/member.module';
-import { SummaryBotModule } from './summary-bot/summary-bot.module';
 import { FollowsModule } from './follows/follows.module';
 import { PostsModule } from './posts/posts.module';
-import { CommunitiesModule } from './communities/communities.module';
 
 @Module({
-  imports: [DatabaseModule, CommunitiesModule, PostsModule, FollowsModule, SummaryBotModule, MemberModule, RoleModule],
+  imports: [
+    DatabaseModule,
+    CommunitiesModule,
+    PostsModule,
+    FollowsModule,
+    MemberModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
