583b4988d90c82f7c1805657b2519950670482b5
vjaviertaype <168123371+vjaviertaype@users.noreply.github.com>
2025-10-25 03:44:08 -0300

    changes(config): se agregaron configuraciones

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { AppLogger } from './utils/app.logger';
+import { AllExceptionsFilter } from './utils/all-exceptions.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT ?? 3000;
+  const logger = new AppLogger();
+  const app = await NestFactory.create(AppModule, { logger });
+  
   app.enableCors();
-  await app.listen(process.env.PORT ?? 3000);
+
+  app.useGlobalFilters(new AllExceptionsFilter(logger));
+  
+  
+  await app.listen(port);
+  logger.log(`Application is running on: ${process.env.HOST ?? 'http://localhost'}:${port}`);
 }
+
 bootstrap();
