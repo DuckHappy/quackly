85f97d8a54b71409b9332b6618d1b23ef2d4ebbb
Sabrina Ortiz <saabri.789@gmail.com>
2025-10-30 13:52:59 -0300

    fix(users): change user messages

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,19 +9,19 @@ export class UsersController {
 
   @Get()
   getUsers() {
-    return "This action returns all users";
+    return "Esta acción devuelve todos los usuarios";
   }
 
   @Post('register')
   async register(@Body() createUserDto: CreateUserDto) {
     if (!createUserDto.email || !createUserDto.username || !createUserDto.password) {
-      throw new BadRequestException('No data provided');
+      throw new BadRequestException('No se permiten campos vacíos');
     }
     const user = await this.userService.register(createUserDto);
     if (!user) {
-      throw new BadRequestException('Email or username already exists');
+      throw new BadRequestException('El correo electrónico o el nombre de usuario ya existen');
     }
-    return { message: 'User registered successfully', user };
+    return { message: 'Registro exitoso', user };
     }
   
  
@@ -29,7 +29,7 @@ export class UsersController {
   async login(@Body() body: { email: string; password: string }) {
     const token = await this.userService.login(body.email, body.password);
     if (!token) {
-      throw new BadRequestException('Invalid email or password');
+      throw new BadRequestException('Credenciales inválidas');
     }
     return { access_token: token };
   }
@@ -38,7 +38,7 @@ export class UsersController {
   async getUserById(@Param('id') id: string) { // obtiene parametro en esta caso id y lo guarda en variable id
     const user = await this.userService.findById(+id); // encontrar usuario por id usando userService
     if (!user) { // si el usuario no existe
-      throw new NotFoundException('User not found'); // muestra mensaje 'User not found'
+      throw new NotFoundException('Usuario no encontrado'); // muestra mensaje 'User not found'
     }
     const { password, ...userWithoutPassword } = user; // extrae la contraseña del usuario
     return userWithoutPassword; // devuelve el usuario encontrado sin la contraseña
@@ -48,10 +48,10 @@ export class UsersController {
   async editUser(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     const user = await this.userService.findById(+id);
     if (!user) {
-      throw new NotFoundException('User not found');
+      throw new NotFoundException('Usuario no encontrado');
     }
     const updatedUser = await this.userService.update(+id, updateUserDto);
-    return { message: 'User updated successfully', user: updatedUser };
+    return { message: 'Usuario actualizado con éxito', user: updatedUser };
   }
 
 }
